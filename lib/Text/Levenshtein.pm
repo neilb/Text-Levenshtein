fec90a5733fde7654e8206d321141be6e98bd308
Neil Bowers <neil@bowers.com>
2014-04-23 22:08:24 +0100

    Replaced UTF-8 dash character with regular one

--- lib/Text/Levenshtein.pm
+++ lib/Text/Levenshtein.pm
@@ -135,7 +135,7 @@ L<Text::LevenshteinXS> and L<Text::Levenshtein::XS> are both versions
 of the Levenshtein algorithm that require a C compiler,
 but will be a lot faster than this module.
 
-The Damerau–Levenshtein edit distance is like the Levenshtein distance,
+The Damerau-Levenshtein edit distance is like the Levenshtein distance,
 but in addition to insertion, deletion and substitution, it also
 considers the transposition of two adjacent characters to be a single edit.
 The module L<Text::Levenshtein::Damerau> defaults to using a pure perl
